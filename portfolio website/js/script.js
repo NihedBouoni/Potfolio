11a8b46ceca12a277cf45d916292f1d37e9487d5
Nihed Bouoni <101837888+NihedBouoni@users.noreply.github.com>
2022-06-05 09:55:04 +0100

    Add files via upload

---
+++ portfolio website/js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing", {
+    strings: ["  ", "...مرحبا", "HELLO...", "salut...", "Hola...", "Guten Tag...", "你好...", "Olá...", "Hej...", "salve...", "안녕하세요..."],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
